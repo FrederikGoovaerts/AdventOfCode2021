df74dcc52ec3b8f83cbd1374da1e607e2d65b9e1
Frederik Goovaerts <info@frederik.dev>
2021-12-11 07:11:57 +0100

    Update day 9 to use neighbour utils

--- src/9/solution.ts
+++ src/9/solution.ts
@@ -1,26 +1,12 @@
 import { asList } from "../utils/inputReader";
+import { getNeighbours, getNeighboursLocs } from "../utils/neighbours";
 
 const input = asList("input").map((val) => val.split("").map(Number));
 const width = input[0].length;
 const height = input.length;
 
-function getNeighbours(x: number, y: number): number[] {
-  return getNeighboursLocs(x, y).map((val) => input[val[0]][val[1]]);
-}
-
-function getNeighboursLocs(x: number, y: number): [number, number][] {
-  const result: [number, number][] = [
-    [x - 1, y],
-    [x, y - 1],
-    [x + 1, y],
-    [x, y + 1],
-  ];
-
-  return result.filter((val) => input[val[0]]?.[val[1]] !== undefined);
-}
-
 function isLowPoint(x: number, y: number): boolean {
-  const neighbours = getNeighbours(x, y);
+  const neighbours = getNeighbours(x, y, input, false);
   return neighbours.find((val) => val <= input[x][y]) === undefined;
 }
 
@@ -34,7 +20,7 @@ function basinSize(x: number, y: number): number {
       checked.add(`${curr[0]}.${curr[1]}`);
       if (input[curr[0]][curr[1]] != 9) {
         size++;
-        locs.push(...getNeighboursLocs(curr[0], curr[1]));
+        locs.push(...getNeighboursLocs(curr[0], curr[1], input, false));
       }
     }
   }
